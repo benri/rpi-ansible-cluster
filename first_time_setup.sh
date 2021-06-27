eab7c462d95829cee308449313cc199588d574f0
benri <benri@users.noreply.github.com>
2021-06-26 18:35:36 -0700

    fix usb setup

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -6,4 +6,6 @@ ansible-playbook playbooks/update_install.yaml
 
 ansible-playbook playbooks/plex_server_setup.yaml
 
-ansible-playbook playbooks/plex_usb_samba.yaml --ask-vault-pass
+ansible-playbook playbooks/plex_usb_setup.yaml --ask-vault-pass
+
+ansible-playbook playbooks/plex_samba_setup.yaml --ask-vault-pass

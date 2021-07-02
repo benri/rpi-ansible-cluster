3681f803d481fdf8d7f0bb132fde4c79ee920901
benri <benri@users.noreply.github.com>
2021-07-02 15:15:13 -0700

    microk8 playbook

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-ansible-playbook playbooks/static_ip.yaml -i tmp/inventory --ask-vault-pass
+# Install required ansible modules
+ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install community.general
 
+ansible-playbook playbooks/static_ip.yaml -i tmp/inventory --ask-vault-pass
 ansible-playbook playbooks/update_install.yaml
 
+# plex
 ansible-playbook playbooks/plex_server_setup.yaml
-
 ansible-playbook playbooks/plex_usb_setup.yaml --ask-vault-pass
-
 ansible-playbook playbooks/plex_samba_setup.yaml --ask-vault-pass
+
+# microk8s
+ansible-playbook playbooks/install_microk8s.yaml
